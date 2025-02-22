82f40e55f6b9124cd1e4d8a80475349fc58c35fb
Swayam <120245434+swayam555@users.noreply.github.com>
2025-02-23 02:39:59 +0530

    app.routes.js

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -5,3 +5,4 @@ const app_controller_1 = require("../controllers/app.controller");
 const router = (0, express_1.Router)();
 router.get("/status", app_controller_1.getStatus);
 exports.default = router;
+ 
